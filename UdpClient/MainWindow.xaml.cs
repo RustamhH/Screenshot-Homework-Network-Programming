eee9ef30afb749aa084c6ae09d48278acf138edf
RustamhH <123383618+RustamhH@users.noreply.github.com>
2023-11-27 21:47:46 +0400

    first

---
+++ UdpClient/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UdpClient
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+
+            Task.Run(() =>
+            {
+                var listener = new Socket(AddressFamily.InterNetwork,
+                          SocketType.Dgram,
+                          ProtocolType.Udp);
+
+
+                var Ip = IPAddress.Parse("192.168.100.8");
+                var Port = 54709;
+
+                var listenerEP = new IPEndPoint(Ip, Port);
+
+                listener.Bind(listenerEP);
+
+                var msg = "";
+                var len = 0;
+                var buffer = new byte[ushort.MaxValue - 29];
+
+
+                EndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+                listener.ReceiveFrom(buffer,ref remoteEP);
+
+                
+            }); 
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var client = new Socket(AddressFamily.InterNetwork,
+                          SocketType.Dgram,
+                          ProtocolType.Udp);
+
+
+            var Ip = IPAddress.Parse("192.168.100.8");
+            var Port = 54709;
+
+            var remoteEP = new IPEndPoint(Ip, Port);
+
+            var len = 0;
+            var buffer = Array.Empty<byte>();
+
+      
+            buffer = Encoding.Default.GetBytes(txtbx.Text);
+            client.SendTo(buffer, remoteEP);
+
+            
+        }
+    }
+}
---
+++ udpServerr/Program.cs
@@ -0,0 +1,122 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace udpServerr
+{
+    internal class Program
+    {
+        static int imagecounter = 0;
+        static DirectoryInfo di = Directory.CreateDirectory($"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}/ScreenShots");
+
+
+
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        private static extern bool GetWindowRect(IntPtr hWnd, out RECT rect);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+
+
+        static Bitmap CaptureAndSaveScreenshot()
+        {
+            IntPtr handle = GetForegroundWindow();
+
+            if (GetWindowRect(handle, out RECT rect))
+            {
+                int width = rect.Right - rect.Left;
+                int height = rect.Bottom - rect.Top;
+
+                using (Bitmap bmp = new Bitmap(width, height))
+                {
+                    using (Graphics g = Graphics.FromImage(bmp))
+                    {
+                        g.CopyFromScreen(rect.Left, rect.Top, 0, 0, new Size(width, height));
+                    }
+
+                    if (File.Exists("last index.txt")) imagecounter = Convert.ToInt32(File.ReadAllText("last index.txt"));
+                    string fileName = $@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}/{di.Name}/Screenshot_{++imagecounter}.png";
+
+                    bmp.Save(fileName, ImageFormat.Png);
+                    File.WriteAllText("last index.txt", imagecounter.ToString());
+
+                    return bmp;
+
+
+                }
+
+            }
+            else
+            {
+                return null;
+            }
+            
+        }
+
+       
+        static void Main(string[] args)
+        {
+            var listener = new Socket(AddressFamily.InterNetwork,
+                          SocketType.Dgram,
+                          ProtocolType.Udp);
+
+
+            var Ip = IPAddress.Parse("192.168.100.8");
+            var Port = 54709;
+
+            var listenerEP = new IPEndPoint(Ip, Port);
+
+            listener.Bind(listenerEP);
+
+            var msg = "";
+            var len = 0;
+            var buffer = new byte[ushort.MaxValue - 29];
+
+
+            EndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+
+
+            
+            len = listener.ReceiveFrom(buffer, ref remoteEP);
+
+            msg = Encoding.Default.GetString(buffer, 0, len);
+            if(msg=="Salam")
+            {
+                Bitmap image =CaptureAndSaveScreenshot();
+                using MemoryStream memoryStream = new MemoryStream();
+                image.Save(memoryStream, ImageFormat.Png);
+                byte[] imageinbytes= memoryStream.ToArray();
+                
+                var client = new Socket(AddressFamily.InterNetwork,
+                           SocketType.Dgram,
+                           ProtocolType.Udp);
+
+
+
+
+                
+                var serverEndPoint = new IPEndPoint(Ip, Port);
+
+                client.SendTo(imageinbytes,serverEndPoint);
+
+            }
+            Console.WriteLine($"{remoteEP} : {msg}");
+            
+        }
+    }
+}
